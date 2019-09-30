2f7b3584352ecafe91568f3929d064f7175876c5
hwuehn <h.wuehn@posteo.de>
2019-09-30 19:02:33 +0200

    feature: added AccountManagementPanel

--- src/de.home.moneystream/application/Main.java
+++ src/de.home.moneystream/application/Main.java
@@ -15,6 +15,8 @@ public class Main {
         frame.pack();
         frame.setVisible(true);
 
+
+
     }
 
 }
---
+++ src/de.home.moneystream/presentation/AccountManagementFrame.java
@@ -0,0 +1,15 @@
+package presentation;
+
+import javax.swing.*;
+
+public class AccountManagementFrame {
+    private JPanel accountManagementFrame;
+    private JComboBox comboBox1;
+    private JButton kontoErstellenIndividuellButton;
+    private JButton kontoHinzufügenButton;
+    private JComboBox comboBox2;
+
+    public JPanel getAccountManagementFrame() {
+        return accountManagementFrame;
+    }
+}
